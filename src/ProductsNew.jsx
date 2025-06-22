5724a63416c2c73f1d08a1c6d4bfa244111681ee
KelvinCastle13 <kelvincastle13@gmail.com>
2025-06-22 19:18:15 -0400

    create action

---
+++ src/ProductsNew.jsx
@@ -0,0 +1,30 @@
+export function ProductsNew({onCreate}) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+     const params = new FormData(form);
+     const successCallback = () => form.reset();
+     onCreate(params, successCallback);
+  };
+
+  return (
+    <div>
+      <h1>New Product</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input name="name" type="text" />
+        </div>
+        <div>
+          Url: <input name="price" type="text" />
+        </div>
+        <div>
+          Width: <input name="description" type="text" />
+        </div>
+        <div>
+          Height: <input name="supplier_id" type="text" />
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+}
--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,7 +1,37 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+import { ProductsIndex } from "./ProductsIndex";
+import { ProductsNew } from "./ProductsNew";
+
 export function ProductsPage() {
+    const [products, setProducts] = useState([]);
+
+    const handleIndex = () => {
+      console.log("handleIndex");
+      axios.get("/Products.json").then((response) => {
+        console.log(response.data);
+        setProducts(response.data);
+      });
+    };
+
+    const handleCreate = (params, successCallback) => {
+      console.log("handleCreate");
+      axios.post("/products.json", params).then((response) => {
+        setProducts([...products, response.data]);
+        successCallback();
+      })
+    };
+
+    const handleShow = (products) => {
+      console.log("handeShow", products);
+    };
+
+    useEffect(handleIndex, []);
+
   return (
     <main>
-      <h1>Welcome to React!</h1>
+      <ProductsNew onCreate={handleCreate}/>
+      <ProductsIndex products={products} onShow={handleShow}/>
     </main>
   )
 }
