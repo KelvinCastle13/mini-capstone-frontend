03bee88d8a9b8c880b3b9e2524cfe6067cc2e71f
KelvinCastle13 <kelvincastle13@gmail.com>
2025-07-07 21:25:50 -0400

    attemp #2

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 // const API_BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://mini-capstone-api-v7ht.onrender.com/products.json";
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mini-capstone-api-v7ht.onrender.com/products.json";
+// axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mini-capstone-api-v7ht.onrender.com/products.json";
 
 
 createRoot(document.getElementById('root')).render(
